e5123006494168317688f9609ef6fc6e963a540d
Andika <andika.kavin@gmail.com>
2023-01-31 21:48:26 +0700

    init repo

---
+++ layouts/home.tsx
@@ -0,0 +1,11 @@
+import Hero from "@components/home/hero"
+import Navbar from "@components/home/navbar"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
